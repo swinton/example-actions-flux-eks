128a096e505f72eec7d0761c28242b2a260d1ec6
Steve Winton <stevewinton@gmail.com>
2020-02-26 17:24:09 -0600

    Align README and script

--- script/install-flux-on-cluster.sh
+++ script/install-flux-on-cluster.sh
@@ -4,7 +4,8 @@ set -e
 
 cd $( dirname "$0" )/..
 
-export GHUSER=swinton # TODO replace with your login
+export GHOWNER=<github user or organization account where your fork lives> # TODO replace with your user login or organization handle
+export GHUSER=<github user that flux commits will be associated with> # TODO replace with your login login
 export GHREPO=example-actions-flux-eks
 
 kubectl create ns flux
@@ -12,6 +13,6 @@ kubectl create ns flux
 fluxctl install \
     --git-user=${GHUSER} \
     --git-email=${GHUSER}@users.noreply.github.com \
-    --git-url=git@github.com:${GHUSER}/${GHREPO}.git \
+    --git-url=git@github.com:${GHOWNER}/${GHREPO}.git \
     --git-path=manifests \
     --namespace=flux | kubectl apply -f -
